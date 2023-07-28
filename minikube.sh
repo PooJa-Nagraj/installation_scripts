21ba12406bf07ccd25bf76447f719e7e87a04e77
PooJa-Nagraj <84430857+PooJa-Nagraj@users.noreply.github.com>
2023-07-28 08:49:47 +0530

    Update minikube.sh

--- minikube.sh
+++ minikube.sh
@@ -7,7 +7,20 @@ sudo apt-get install curl wget apt-transport-https virtualbox virtualbox-ext-pac
 
 echo "1st install docker"
 
-sudo apt update && apt -y install docker.io
+// sudo apt update && apt -y install docker.io
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo systemctl start docker
 sudo systemctl enable docker
