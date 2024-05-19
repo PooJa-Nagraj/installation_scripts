c0c147898a679ffca1aa69ddab361a941ae14dfa
PooJa-Nagraj <84430857+PooJa-Nagraj@users.noreply.github.com>
2024-05-19 13:38:00 +0530

    Update sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,3 +1,15 @@
 # INSTALL SONARQUBE
   
     docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
+
+
+#Install locally
+
+apt install unzip
+adduser sonarqube
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.4.0.54424.zip
+unzip *
+chmod -R 755 /home/sonarqube/sonarqube-9.4.0.54424
+chown -R sonarqube:sonarqube /home/sonarqube/sonarqube-9.4.0.54424
+cd sonarqube-9.4.0.54424/bin/linux-x86-64/
+./sonar.sh start
